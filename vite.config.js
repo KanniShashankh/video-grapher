ae80330223b553cc0b173973fa1e2f3360ffe2f3
Shashankh Kanni <shashank.kanni@gmail.com>
2023-03-18 16:48:08 +0530

    use local COOP

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
+
+const crossOriginIsolation = () => ({
+  name: 'configure-server',
+
+  configureServer(server) {
+    server.middlewares.use((_req, res, next) => {
+      res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+      res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+      res.setHeader("Cross-Origin-Resource-Policy", "same-origin");
+      next();
+    });
+  }
+
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: 
     [
       react(),
-      crossOriginIsolation()
+      crossOriginIsolation(),
     ],
 })
