40614c299dfa2646010b35b568ea5c7247024632
Shashankh Kanni <shashank.kanni@gmail.com>
2023-03-21 20:29:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-const crossOriginIsolation = () => ({
-  name: 'configure-server',
-
-  configureServer(server) {
-    server.middlewares.use((_req, res, next) => {
-      res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-      res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-      res.setHeader("Cross-Origin-Resource-Policy", "same-origin");
-      next();
-    });
-  }
-
-});
+import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
