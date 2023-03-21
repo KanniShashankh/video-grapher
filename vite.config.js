b4f9ec53f7ba524029431b7aded85535ad1e3075
Shashankh Kanni <shashank.kanni@gmail.com>
2023-03-21 20:53:52 +0530

    first

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
 export default defineConfig({
   plugins: 
     [
-      react(),
       crossOriginIsolation(),
+      react(),
     ],
 })
