43813595de95350ec6c0735c4128f8469237e425
Shashankh Kanni <shashank.kanni@gmail.com>
2023-08-11 23:22:12 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
+    }
+  },
   plugins: 
     [
       crossOriginIsolation(),
