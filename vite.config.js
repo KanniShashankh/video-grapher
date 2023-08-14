68ef03a03326996a559dac4acec9d8fe20fa060b
Shashankh Kanni <shashank.kanni@gmail.com>
2023-08-14 22:44:43 +0530

    CORS

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    headers: {
-      "Cross-Origin-Embedder-Policy": "require-corp",
-      "Cross-Origin-Opener-Policy": "same-origin",
-    }
-  },
-  plugins: 
-    [
-      react(),
-    ],
-})
+  plugins: [
+    {
+      name: "configure-response-headers",
+      configureServer: (server) => {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+          next();
+        });
+      },
+    },
+  ],
+});
